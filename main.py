a534d7594b0c4a3c156b277eb4ff9013cf6a4c3e
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2021-12-14 11:55:29 +0330

    dfsd

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 import crud
 
 app=FastAPI()
 
+
+class Buy(BaseModel):
+    quantity: int
+    token: str
+    buyPercent: float
+
+class Sell(BaseModel):
+    quantity: int
+    token: str
+    sellPercent: float
+
 @app.post("/buy/")
-def buy(quantity:int,token:str, buyPercent:float):
-    nobitex_buy=crud.buy(quantity=quantity,token=token,buyPercent=buyPercent)
+def buy(buy:Buy):
+    nobitex_buy=crud.buy(quantity=buy.quantity,token=buy.token,buyPercent=buy.buyPercent)
 
     return nobitex_buy
 
 
 @app.post("/sell/")
-def buy(quantity:int,token:str, sellPercent:float):
-    nobitex_sell=crud.sell(quantity=quantity,token=token,sellPercent=sellPercent)
+def sell(sell:Sell):
+    nobitex_sell=crud.sell(quantity=sell.quantity,token=sell.token,sellPercent=sell.sellPercent)
 
     return nobitex_sell
