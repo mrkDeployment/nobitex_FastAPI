a23c0bec8ea0d62507e22c8840f78236313bce5a
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2021-12-16 00:34:02 +0330

    dsd

--- crud.py
+++ crud.py
@@ -7,230 +7,118 @@ def tether_price():
 
     return tether
 
-async def buy(quantity: int, token: str, buyPercent: float=10):
+def buy(quantity: int, token: str,
+        nobitex_coin: str,
+        nobitex_coin_percent:int,
+        binance_coin: str,
+        coin:str,
+        buyPercent: float=10):
 
-    coin_list = [
-      "btc",
-      "eth",
-      "ltc",
-      "shib",
-      "xlm",
-      "trx",
-      "doge",
-      "etc",
-      "bnb",
-      "eos",
-      "xrp",
-      "uni",
-      "link",
-      "dot",
-      "aave",
-      "ada"
-    ]
-
-    nobitex_coin_list = [
-      ["BTCIRT",1],
-      ["ETHIRT",1],
-      ["LTCIRT",1],
-      ["SHIBIRT",1000],
-      ["XLMIRT",1],
-      ["TRXIRT",1],
-      ["DOGEIRT",1],
-      ["ETCIRT",1],
-      ["BNBIRT",1],
-      ["EOSIRT",1],
-      ["XRPIRT",1],
-      ["UNIIRT",1],
-      ["LINKIRT",1],
-      ["DOTIRT",1],
-      ["AAVEIRT",1],
-      ["ADAIRT",1]
-    ]
-
-    binance_coin_list = [
-      "BTCUSDT",
-      "ETHUSDT",
-      "LTCUSDT",
-      "SHIBUSDT",
-      "XLMUSDT",
-      "TRXUSDT",
-      "DOGEUSDT",
-      "ETCUSDT",
-      "BNBUSDT",
-      "EOSUSDT",
-      "XRPUSDT",
-      "UNIUSDT",
-      "LINKUSDT",
-      "DOTUSDT",
-      "AAVEUSDT",
-      "ADAUSDT"
-    ]
-
-    for i in range(1,len(nobitex_coin_list)+1):
-      j=i-1
-
-      response1=await requests.get('https://api.nobitex.ir/v2/orderbook/' + nobitex_coin_list[i - 1][0])
-      response= response1.json()
-      print("eeeeee",response)
-
-      price_sum_bid = 0
-
-      tether=float(tether_price())
-
-      nobitex_price_bid={}
-
-      for k in range(16):
-        price_sum_bid += float(response["bids"][k][0]) * float(response["bids"][k][1])
-
-        if (quantity * 3 < price_sum_bid):
-          price_bid = float(response["bids"][k][0]) / tether
-          nobitex_price_bid[j]= price_bid / nobitex_coin_list[j][1]
-          break
-        else:
-          if (k == 15):
-            price_bid = float(response["bids"][15][0]) * 1.03 / tether
-            nobitex_price_bid[j]: price_bid / nobitex_coin_list[j][1]
-
-      binance_price={}
-      print('sssssssssss')
-      response1=await requests.get("https://api.binance.com/api/v3/ticker/price?symbol="+binance_coin_list[j])
-      print('dddddddddddddd')
-      response=response1.json()
-      price= response["price"]
-      binance_price[j]= float(price)
-
-      response_buy={}
-
-      if ((binance_price[j]-nobitex_price_bid[j]) / binance_price[j])*100 > buyPercent:
-
-        allowed_price = tether * 0.9875 * binance_price[j]
-        amount = str(quantity / (allowed_price * nobitex_coin_list[j][1]))
-        config = {"Authorization": "Token " + token}
-        buy_data = {
-          "type": "buy",
-          "execution": "market",
-          "srcCurrency": coin_list[j],
-          "dstCurrency": "rls",
-          "amount": amount,
-        }
-
-        url="https://api.nobitex.ir/market/orders/add"
-        response1 = await requests.post(url, headers=config, data=buy_data)
-        response_buy[j]=response1.json()
-
-
-    return response_buy
-
-
-
-
-async def sell(quantity: int, token: str, sellPercent: float = 10):
-  coin_list = [
-    "btc",
-    "eth",
-    "ltc",
-    "shib",
-    "xlm",
-    "trx",
-    "doge",
-    "etc",
-    "bnb",
-    "eos",
-    "xrp",
-    "uni",
-    "link",
-    "dot",
-    "aave",
-    "ada"
-  ]
-
-  nobitex_coin_list = [
-    ["BTCIRT", 1],
-    ["ETHIRT", 1],
-    ["LTCIRT", 1],
-    ["SHIBIRT", 1000],
-    ["XLMIRT", 1],
-    ["TRXIRT", 1],
-    ["DOGEIRT", 1],
-    ["ETCIRT", 1],
-    ["BNBIRT", 1],
-    ["EOSIRT", 1],
-    ["XRPIRT", 1],
-    ["UNIIRT", 1],
-    ["LINKIRT", 1],
-    ["DOTIRT", 1],
-    ["AAVEIRT", 1],
-    ["ADAIRT", 1]
-  ]
-
-  binance_coin_list = [
-    "BTCUSDT",
-    "ETHUSDT",
-    "LTCUSDT",
-    "SHIBUSDT",
-    "XLMUSDT",
-    "TRXUSDT",
-    "DOGEUSDT",
-    "ETCUSDT",
-    "BNBUSDT",
-    "EOSUSDT",
-    "XRPUSDT",
-    "UNIUSDT",
-    "LINKUSDT",
-    "DOTUSDT",
-    "AAVEUSDT",
-    "ADAUSDT"
-  ]
-
-  for i in range(1, len(nobitex_coin_list) + 1):
-    j = i - 1
-
-    response1 = await requests.get('https://api.nobitex.ir/v2/orderbook/' + nobitex_coin_list[i - 1][0])
-    response = response1.json()
-    print("eeeeee", response)
+    response1=requests.get('https://api.nobitex.ir/v2/orderbook/' + nobitex_coin)
+    response= response1.json()
+    print("eeeeee",response)
 
     price_sum_bid = 0
 
-    tether = float(tether_price())
+    tether=float(tether_price())
 
-    nobitex_price_bid = {}
+    nobitex_price_bid={}
 
     for k in range(16):
       price_sum_bid += float(response["bids"][k][0]) * float(response["bids"][k][1])
 
       if (quantity * 3 < price_sum_bid):
         price_bid = float(response["bids"][k][0]) / tether
-        nobitex_price_bid[j] = price_bid / nobitex_coin_list[j][1]
+        nobitex_price_bid= price_bid / nobitex_coin_percent
         break
       else:
         if (k == 15):
           price_bid = float(response["bids"][15][0]) * 1.03 / tether
-          nobitex_price_bid[j]: price_bid / nobitex_coin_list[j][1]
+          nobitex_price_bid: price_bid / nobitex_coin_percent
 
-    binance_price = {}
+    binance_price={}
     print('sssssssssss')
-    response1 = await requests.get("https://api.binance.com/api/v3/ticker/price?symbol=" + binance_coin_list[j])
+    response1= requests.get("https://api.binance.com/api/v3/ticker/price?symbol="+binance_coin)
     print('dddddddddddddd')
-    response = response1.json()
-    price = response["price"]
-    binance_price[j] = float(price)
+    response=response1.json()
+    price= response["price"]
+    binance_price= float(price)
 
-    response_sell = {}
+    response_buy={}
 
-    if ((nobitex_price_bid[j]-binance_price[j]) / binance_price[j]) * 100 > sellPercent:
-      allowed_price = tether * binance_price[j]
-      amount = str(quantity / (allowed_price * nobitex_coin_list[j][1]))
+    if ((binance_price-nobitex_price_bid) / binance_price)*100 > buyPercent:
+
+      allowed_price = tether * 0.9875 * binance_price
+      amount = str(quantity / (allowed_price * nobitex_coin_percent))
       config = {"Authorization": "Token " + token}
-      sell_data = {
-        "type": "sell",
+      buy_data = {
+        "type": "buy",
         "execution": "market",
-        "srcCurrency": coin_list[j],
+        "srcCurrency": coin,
         "dstCurrency": "rls",
         "amount": amount,
       }
 
-      url = "https://api.nobitex.ir/market/orders/add"
-      response1 = requests.post(url, headers=config, data=sell_data)
-      response_sell[j] = response1.json()
+      url="https://api.nobitex.ir/market/orders/add"
+      response1 = requests.post(url, headers=config, data=buy_data)
+      response_buy=response1.json()
+
+
+    return response_buy
+
+
+def sell(quantity: int, token: str,
+        nobitex_coin: str,
+        nobitex_coin_percent: int,
+        binance_coin: str,
+        coin: str,
+        sellPercent: float = 10):
+  response1 = requests.get('https://api.nobitex.ir/v2/orderbook/' + nobitex_coin)
+  response = response1.json()
+  print("eeeeee", response)
+
+  price_sum_bid = 0
+
+  tether = float(tether_price())
+
+  nobitex_price_bid = {}
+
+  for k in range(16):
+    price_sum_bid += float(response["bids"][k][0]) * float(response["bids"][k][1])
+
+    if (quantity * 3 < price_sum_bid):
+      price_bid = float(response["bids"][k][0]) / tether
+      nobitex_price_bid = price_bid / nobitex_coin_percent
+      break
+    else:
+      if (k == 15):
+        price_bid = float(response["bids"][15][0]) * 1.03 / tether
+        nobitex_price_bid: price_bid / nobitex_coin_percent
+
+  binance_price = {}
+  print('sssssssssss')
+  response1 = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=" + binance_coin)
+  print('dddddddddddddd')
+  response = response1.json()
+  price = response["price"]
+  binance_price = float(price)
+
+  response_sell = {}
+
+  if ((nobitex_price_bid - binance_price) / binance_price) * 100 > sellPercent:
+    allowed_price = tether * 0.9875 * binance_price
+    amount = str(quantity / (allowed_price * nobitex_coin_percent))
+    config = {"Authorization": "Token " + token}
+    sell_data = {
+      "type": "sell",
+      "execution": "market",
+      "srcCurrency": coin,
+      "dstCurrency": "rls",
+      "amount": amount,
+    }
+
+    url = "https://api.nobitex.ir/market/orders/add"
+    response1 = requests.post(url, headers=config, data=sell_data)
+    response_sell = response1.json()
 
   return response_sell
--- main.py
+++ main.py
@@ -6,89 +6,44 @@ import crud
 
 app=FastAPI()
 
-
 class Buy(BaseModel):
     quantity: int
     token: str
+    nobitex_coin: str
+    nobitex_coin_percent: int
+    binance_coin: str
+    coin: str
     buyPercent: float
 
 class Sell(BaseModel):
     quantity: int
     token: str
+    nobitex_coin: str
+    nobitex_coin_percent: int
+    binance_coin: str
+    coin: str
     sellPercent: float
 
 @app.post("/buy/")
 def buy(buy:Buy):
-    nobitex_buy=crud.buy(quantity=buy.quantity,token=buy.token,buyPercent=buy.buyPercent)
+    nobitex_buy=crud.buy(quantity=buy.quantity,
+                         token=buy.token,
+                         buyPercent=buy.buyPercent,
+                         nobitex_coin=buy.nobitex_coin,
+                         binance_coin=buy.binance_coin,
+                         coin=buy.coin,
+                         nobitex_coin_percent=buy.nobitex_coin_percent)
 
     return nobitex_buy
 
 
 @app.post("/sell/")
 def sell(sell:Sell):
-    nobitex_sell=crud.sell(quantity=sell.quantity,token=sell.token,sellPercent=sell.sellPercent)
-
+    nobitex_sell=crud.sell(quantity=sell.quantity,
+                         token=sell.token,
+                         sellPercent=sell.sellPercent,
+                         nobitex_coin=sell.nobitex_coin,
+                         binance_coin=sell.binance_coin,
+                         coin=sell.coin,
+                         nobitex_coin_percent=sell.nobitex_coin_percent)
     return nobitex_sell
-
-@app.get("/sedssd/")
-def cdd():
-  coin_list = [
-    "btc",
-    "eth",
-    "ltc",
-    "shib",
-    "xlm",
-    "trx",
-    "doge",
-    "etc",
-    "bnb",
-    "eos",
-    "xrp",
-    "uni",
-    "link",
-    "dot",
-    "aave",
-    "ada"
-  ]
-
-  nobitex_coin_list = [
-    ["BTCIRT", 1],
-    ["ETHIRT", 1],
-    ["LTCIRT", 1],
-    ["SHIBIRT", 1000],
-    ["XLMIRT", 1],
-    ["TRXIRT", 1],
-    ["DOGEIRT", 1],
-    ["ETCIRT", 1],
-    ["BNBIRT", 1],
-    ["EOSIRT", 1],
-    ["XRPIRT", 1],
-    ["UNIIRT", 1],
-    ["LINKIRT", 1],
-    ["DOTIRT", 1],
-    ["AAVEIRT", 1],
-    ["ADAIRT", 1]
-  ]
-
-  binance_coin_list = [
-    "BTCUSDT",
-    "ETHUSDT",
-    "LTCUSDT",
-    "SHIBUSDT",
-    "XLMUSDT",
-    "TRXUSDT",
-    "DOGEUSDT",
-    "ETCUSDT",
-    "BNBUSDT",
-    "EOSUSDT",
-    "XRPUSDT",
-    "UNIUSDT",
-    "LINKUSDT",
-    "DOTUSDT",
-    "AAVEUSDT",
-    "ADAUSDT"
-  ]
-  print('aaaaaaaaaa')
-  response1 = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=" + binance_coin_list[0])
-  response=response1.json()
-  return response
