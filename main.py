e430003f688210dce6535cf65aa7446e00f61b18
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2021-12-16 01:07:53 +0330

    dsd

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from pydantic import BaseModel
 import requests
 
 import crud
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class Buy(BaseModel):
     quantity: int
